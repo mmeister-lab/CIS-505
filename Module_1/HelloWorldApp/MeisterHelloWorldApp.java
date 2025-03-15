32a80039ff7c3dafff46589398fadab3556670b0
Mike <mmeister@my365.bellevue.edu>
2025-03-15 06:23:38 -0500

    “Assignment.”

---
+++ Module_1/HelloWorldApp/MeisterHelloWorldApp.java
@@ -0,0 +1,11 @@
+/*Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+Comprehensive Version (12th ed.). Pearson Education, Inc.
+*/
+public class MeisterHelloWorldApp{
+	public static void main(String[] args)
+	{
+	String firstname = "Michael";
+	String lastname = "Meister";
+	System.out.printf("Hello World from " + firstname + " " + lastname);
+	}
+}
