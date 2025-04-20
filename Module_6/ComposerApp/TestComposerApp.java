61e14595d1dbfec576e136b84fdd02dcebc05735
Mike <mmeister@my365.bellevue.edu>
2025-04-20 13:52:43 -0500

    Assignment 6.2

--- Module_6/ComposerApp/TestComposerApp.java
+++ Module_6/ComposerApp/TestComposerApp.java
@@ -1,3 +1,5 @@
+/*Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+Comprehensive Version (12th ed.). Pearson Education, Inc*/
 package ComposerApp;
 
 import java.util.List;
