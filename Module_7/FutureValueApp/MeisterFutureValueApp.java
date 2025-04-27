37d3a2114289056b9ca82333d42d3f2c50b653c0
Mike <mmeister@my365.bellevue.edu>
2025-04-27 18:56:22 -0500

    Assignment 7.2.

--- Module_7/FutureValueApp/MeisterFutureValueApp.java
+++ Module_7/FutureValueApp/MeisterFutureValueApp.java
@@ -1,3 +1,4 @@
+
 /*Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
 Comprehensive Version (12th ed.). Pearson Education, Inc*/
 import javafx.scene.paint.Color;
@@ -17,81 +18,90 @@ import javafx.scene.Scene;
 
 public class MeisterFutureValueApp extends Application {
 
-    /*Two TextField nodes */
+    /* Two TextField nodes */
     private TextField tfMessage1 = new TextField();
     private TextField tfMessage2 = new TextField();
-    /*Textarea node*/
+    /* Textarea node */
     private TextArea tArea = new TextArea();
-    /*5 Label nodes*/
-    private Label lblMonthlyPayment = new Label("Monthly Payment:  ");
-    private Label lblInterestRate = new Label("Interest Rate");
+    /* 5 Label nodes */
+    private Label lblMonthlyPayment = new Label("Monthly Payment:");
+    private Label lblInterestRate = new Label("Interest Rate:");
     private Label lblInterestRateFormat = new Label("Enter 11.1% as 11.1");
-    private Label lblYears = new Label("Years");
-    private Label lbl5 = new Label();
-    /*Combobox node*/
+    private Label lblYears = new Label("Years:");
+    private Label lblFutureValueDate = new Label();
+    /* Combobox node */
     private ComboBox<Integer> cbo = new ComboBox<>();
-    /*Calculate Button node*/
+    /* Calculate Button node */
     private Button btnCalculate = new Button("Calculate");
-    /*Clear Button node*/
-    private Button btnClear = new Button();
+    /* Clear Button node */
+    private Button btnClear = new Button("Clear");
 
     @Override
     public void start(Stage primaryStage) {
 
-        /*GridPane arranges nodes in a grid (matrix) formation. */
+        /* GridPane arranges nodes in a grid (matrix) formation. */
         GridPane pane = new GridPane();
-        /*Set node properties*/
+        /* Set node properties */
         pane.setAlignment(Pos.CENTER);
         pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
         pane.setHgap(5.5);
         pane.setVgap(5.5);
 
-        /*Place nodes in the pane */
-        pane.add(tfMessage1, 0, 0);
-        pane.add(tfMessage2, 0, 1);
-        pane.add(tArea, 0, 2);
-        pane.add(lblInterestRate, 0, 3);
-        pane.add(lblMonthlyPayment, 0, 4);
-        pane.add(lblInterestRateFormat, 0, 5);
-        pane.add(lblYears, 0, 6);
-        pane.add(lbl5, 0, 7);
-        pane.add(cbo, 0, 8);
-        pane.add(btnCalculate, 0, 9);
-        pane.add(btnClear, 0, 10);
-
-        /*set the text fill to red */
-        /*Set the font color of the interest rate instructions to red.*/
+        /* Set the font color of the interest rate instructions to red. */
         lblInterestRateFormat.setTextFill(Color.RED);
-        /*Add the label to the grid pane. */
-        pane.add(lblInterestRateFormat, 1, 20);
-        /*position the label to the right of the pane.*/
+
+        /* Add Monthly Payment Label and TextField to first row of grid. */
+        pane.add(lblMonthlyPayment, 0, 0);
+        pane.add(tfMessage1, 1, 0);
+
+        /* Add Interest Rate Label and TextField to second row of grid. */
+        pane.add(lblInterestRate, 0, 1);
+        pane.add(tfMessage2, 1, 1);
+
+        /* Add Interest Rate Format Label to third row of grid. */
+        pane.add(lblInterestRateFormat, 1, 2);
+        /* position the label to the right of the pane. */
         GridPane.setHalignment(lblInterestRateFormat, HPos.RIGHT);
-        /*Create a new HBox container*/
+
+        /* Add years Label ComboBox to fourth row of grid. */
+        pane.add(lblYears, 0, 3);
+        cbo.prefWidthProperty().bind(tfMessage2.widthProperty());
+        pane.add(cbo, 1, 3);
+
+        /* Create a new HBox container for buttons */
         HBox actionBtnContainer = new HBox();
-        /*Set the containers spacing.*/
-        actionBtnContainer.setPadding(new Insets(15, 0, 15, 30)); 
-        /*Set the container's spacing.*/
+        /* Set the containers padding. */
+        actionBtnContainer.setPadding(new Insets(15, 0, 15, 30));
+        /* Set the container's spacing. */
         actionBtnContainer.setSpacing(10);
-        /*Add the btnClear to the container.*/
+        /* Add the btnClear to the container. */
         actionBtnContainer.getChildren().add(btnClear);
-        /*Add the brnCalc to the container.*/
+        /* Add the brnCalc to the container. */
         actionBtnContainer.getChildren().add(btnCalculate);
-        /*Add the container to the GridPanel*/
+        /* Set buttons to right alignment. */
+        actionBtnContainer.setAlignment(Pos.CENTER_RIGHT);
+        /* Add the container to the fifth row of the grid. */
         pane.add(actionBtnContainer, 1, 4);
 
-        /*Create scene and place it in teh stage.*/
-        Scene scene = new Scene(pane);
-        /*Primary stage's title to “Meister Future Value App.”*/
+        /* Add Future Value Date Label to sixth row of grid. */
+        pane.add(lblFutureValueDate, 0, 5);
+
+        /* Add TextArea to the seventh row of the grid spanning two columns. */
+        pane.add(tArea, 0, 6, 2, 1);
+
+        /* Create scene and place it in the stage. */
+        Scene scene = new Scene(pane, 300, 400);
+        /* Primary stage's title to “Meister Future Value App.” */
         primaryStage.setTitle("Meister Future Value App");
-        /*Place the scene in the stage.*/
+        /* Place the scene in the stage. */
         primaryStage.setScene(scene);
-        /*Display the stage*/
+        /* Display the stage */
         primaryStage.show();
     }
 
-   public static void main(String[] args) {
-    
-    Application.launch(args);
-   }
-        
+    public static void main(String[] args) {
+
+        Application.launch(args);
+    }
+
 }
